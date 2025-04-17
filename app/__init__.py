93ebf3b675f9be70f9d382b1b627dc4724f01bac
Lina Martinez <lmcastro3501@gmail.com>
2025-04-17 09:48:47 -0700

    Registers books_bp

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from .routes.book_routes import books_bp
 
 def create_app():
     app = Flask(__name__)
+
+    # Register books blueprint -> our bp is recognized by our Flask app
+    app.register_blueprint(books_bp)
 
     return app
