0a905e53f53b483e136a181976a3a73bac914daf
Lina Martinez <lmcastro3501@gmail.com>
2025-04-17 22:21:37 -0700

    Implements endpoint to get one book.

--- app/models/book.py
+++ app/models/book.py
@@ -2,7 +2,7 @@
 class Book:
     def __init__(self, id, title, description):
         self.id = id
-        self.title = title, 
+        self.title = title
         self.description = description
 
 
--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -15,3 +15,16 @@ def get_all_books():
             }
         )
     return books_response
+
+# Getting a single book endpoint
+@books_bp.get("/<book_id>")
+def gets_one_book(book_id):
+    book_id = int(book_id)
+    for book in books:
+        if book_id == book.id:
+            return dict(
+                id = book.id,
+                title = book.title,
+                description = book.description,
+            )
+
