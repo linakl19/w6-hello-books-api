b559fe822f46ba5d0269082a64869bdc1e1ac4b7
Lina Martinez <lmcastro3501@gmail.com>
2025-04-24 08:25:29 -0700

    Sets database migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from .routes.book_routes import books_bp
 from .db import db, migrate
+from .models import book
+from .routes.book_routes import books_bp
 
 def create_app():
     app = Flask(__name__)
