4cf18c137ee8040c2638a77e306dbef9b8676e5b
Lina Martinez <lmcastro3501@gmail.com>
2025-04-24 07:31:30 -0700

    Defines Book model

--- app/models/book.py
+++ app/models/book.py
@@ -1,3 +1,11 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+
 # # Book class to represent book data
 # class Book:
 #     def __init__(self, id, title, description):
