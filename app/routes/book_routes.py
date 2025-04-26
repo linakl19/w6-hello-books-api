a52b78bae5875d8e401946631f0012b9e031eed0
Lina Martinez <lmcastro3501@gmail.com>
2025-04-26 15:04:29 -0700

    Implements a route that creates a new record of book.

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,7 +1,33 @@
-from flask import Blueprint, abort, make_response
-# from app.models.book import books
+from flask import Blueprint, abort, make_response, request
+from app.models.book import Book
+from ..db import db
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+
+@books_bp.post("")
+def create_book():
+    # Handling the incomming data
+    request_body = request.get_json()
+    title = request_body["title"]
+    description = request_body["description"]
+
+    # Creating and saving the new book
+    new_book = Book(title=title, description=description)
+    db.session.add(new_book)
+    db.session.commit()
+
+    # Returning a response
+    response = {
+        "id": new_book.id,
+        "title": new_book.title,
+        "description": new_book.description,
+    }
+    return response, 201
+
+# from flask import Blueprint, abort, make_response
+# from app.models.book import books
+
+# books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
 # @books_bp.get("")
 # def get_all_books():
