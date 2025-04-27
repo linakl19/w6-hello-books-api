a81150667e0bec42d5283de8bc170b9afd4d8be0
Lina Martinez <lmcastro3501@gmail.com>
2025-04-26 17:54:12 -0700

    Implements getting all books endpoint

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -24,6 +24,26 @@ def create_book():
     }
     return response, 201
 
+#Getting all books endpoint
+@books_bp.get("")
+def get_all_books():
+    query = db.select(Book).order_by(Book.id)
+    books = db.session.scalars(query)
+    # We could also write the line above as:
+    # books = db.session.execute(query).scalars()
+
+    books_response = []
+    for book in books:
+        books_response.append(
+            {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+        )
+    return books_response
+
+
 # from flask import Blueprint, abort, make_response
 # from app.models.book import books
 
