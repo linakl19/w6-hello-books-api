1ea4ee760400dd19eeff724eb7b59e347f66a019
Lina Martinez <lmcastro3501@gmail.com>
2025-05-06 09:24:57 -0700

    defines create_model() as a route utility and test it.

--- tests/test_route_utilities.py
+++ tests/test_route_utilities.py
@@ -1,5 +1,6 @@
 from werkzeug.exceptions import HTTPException
-from app.routes.route_utilities import validate_model
+from app.models.author import Author
+from app.routes.route_utilities import validate_model, create_model
 import pytest
 from app.models.book import Book
 
@@ -28,3 +29,54 @@ def test_validate_book_invalid_id(two_saved_books):
     # cause an `HTTPException` when an `abort` statement is reached 
     with pytest.raises(HTTPException):
         result_book = validate_model(Book, "cat")
+
+# We use the `client` fixture because we need an
+# application context to work with the database session
+def test_create_model_book(client):
+    # Arrange
+    test_data = {
+        "title": "New Book",
+        "description": "The Best!"
+    }
+
+    # Act
+    result, status_code = create_model(Book, test_data)
+
+    # Assert
+    assert status_code == 201
+    assert result == {
+        "id": 1,
+        "title": "New Book",
+        "description": "The Best!"
+    }
+
+def test_create_model_book_missing_data(client):
+    # Arrange
+    test_data = {
+        "description": "The Best!"
+    }
+
+    # Act & Assert
+    # Calling `create_model` without being invoked by a route will
+    # cause an `HTTPException` when an `abort` statement is reached 
+    with pytest.raises(HTTPException) as error:
+        result_book = create_model(Book, test_data)
+
+    response = error.value.response
+    assert response.status == "400 BAD REQUEST"
+
+def test_create_model_author(client):
+    # Arrange
+    test_data = {
+        "name": "New Author"
+    }
+
+    #Act
+    result, status_code = create_model(Author, test_data)
+
+    # Assert
+    assert status_code == 201
+    assert result == {
+        "id": 1,
+        "name": "New Author"
+    }
