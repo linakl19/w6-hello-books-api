6cced71411dc1144f6ac62ece174859262bbdf63
Lina Martinez <lmcastro3501@gmail.com>
2025-05-06 08:17:27 -0700

    implements one to many relationship between Authors and Books.

--- app/models/author.py
+++ app/models/author.py
@@ -1,10 +1,16 @@
 # app/models/author.py
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+# from app.models.book import Book
 from ..db import db
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from .book import Book
 
 class Author(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str]
+    books: Mapped[list["Book"]] = relationship(back_populates="author")
 
     def to_dict(self):
         author_as_dict = {
