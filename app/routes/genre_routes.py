6cdb26cb9f73a20632cbae50790011ef62944ead
Lina Martinez <lmcastro3501@gmail.com>
2025-05-08 16:07:57 -0700

    Implement many to many nested routes in genre_routes.py

--- app/routes/genre_routes.py
+++ app/routes/genre_routes.py
@@ -1,4 +1,5 @@
 from app.models.genre import Genre
+from app.models.book import Book
 from .route_utilities import validate_model, create_model, get_models_with_filters
 from flask import Blueprint, request
 from ..db import db
@@ -14,3 +15,19 @@ def create_genre():
 @bp.get("")
 def get_all_genres():
     return get_models_with_filters(Genre, request.args)
+
+
+# Nested routes - many to many relationship
+@bp.post("/<genre_id>/books")
+def post_book_with_genre_id(genre_id):
+    genre = validate_model(Genre, genre_id)
+    request_body = request.get_json()
+    request_body["genres"] = [genre]
+
+    return create_model(Book, request_body)
+
+
+@bp.get("/<genre_id>/books")
+def get_books_with_genre_id(genre_id):
+    genre = validate_model(Genre, genre_id)
+    return [book.to_dict() for book in genre.books]
