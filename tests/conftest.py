7a2132d56c08de289010046d3644ed7361b3ac4e
Lina Martinez <lmcastro3501@gmail.com>
2025-05-08 12:47:11 -0700

    Add two_saved_genres fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,24 +5,11 @@ from flask.signals import request_finished
 from dotenv import load_dotenv
 import os
 from app.models.book import Book
+from app.models.genre import Genre
+
 
 load_dotenv()
 
-@pytest.fixture
-def two_saved_books(app):
-    # Arrange
-    ocean_book = Book(
-        title="Ocean Book",
-        description="watr 4evr")
-    mountain_book = Book(
-        title="Mountain Book",
-        description="i luv 2 climb rocks")
-
-    db.session.add_all([ocean_book, mountain_book])
-    # Alternatively, we could do
-    # db.session.add(ocean_book)
-    # db.session.add(mountain_book)
-    db.session.commit()
 
 @pytest.fixture
 def app():
@@ -47,3 +34,30 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
+
+
+@pytest.fixture
+def two_saved_books(app):
+    # Arrange
+    ocean_book = Book(
+        title="Ocean Book",
+        description="watr 4evr")
+    mountain_book = Book(
+        title="Mountain Book",
+        description="i luv 2 climb rocks")
+
+    db.session.add_all([ocean_book, mountain_book])
+    # Alternatively, we could do
+    # db.session.add(ocean_book)
+    # db.session.add(mountain_book)
+    db.session.commit()
+
+
+@pytest.fixture
+def two_saved_genres(app):
+    # Arrange
+    non_fiction_genre = Genre(name="Non-fiction")
+    biography_genre = Genre(name="Biography")
+
+    db.session.add_all([biography_genre, non_fiction_genre])
+    db.session.commit()
