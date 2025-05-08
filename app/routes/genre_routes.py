b6cd4695f770f1f79b408bda354591e171c5a94d
Lina Martinez <lmcastro3501@gmail.com>
2025-05-08 13:34:18 -0700

    Define genre enpoints for get and post requests

--- app/routes/genre_routes.py
+++ app/routes/genre_routes.py
@@ -1,7 +1,16 @@
 from app.models.genre import Genre
-from .route_utilities import validate_model, create_model
-from flask import Blueprint
+from .route_utilities import validate_model, create_model, get_models_with_filters
+from flask import Blueprint, request
 from ..db import db
 
 bp = Blueprint("genres_bp", __name__, url_prefix="/genres")
 
+
+@bp.post("")
+def create_genre():
+    request_body = request.get_json()
+    return create_model(Genre, request_body)
+
+@bp.get("")
+def get_all_genres():
+    return get_models_with_filters(Genre, request.args)
